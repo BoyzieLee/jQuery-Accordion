781c15366176d25a389f2bda29cae7644ffa6fb7
Boyzie Mathis <boyzielee@gmail.com>
2016-04-13 16:19:53 -0400

    layout complete. need to work on hidden effect

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
-console.log('Hello New Project!');
+console.log('Accorion!');
+
+
+$('.accorion').addClass('hidden');
+$('.accorion').removeClass('hidden');
+var accordion = $('#HTMLSection', '#SASSSection', '#JSSection');
+
+accordion.on('click', function (event) {
+  event.preventDefault();
+// this calls back the variable that was set earlier
+  hidden.toggleClass('open');
+});
+function animate () {
+  // do some fun animation things
+}
